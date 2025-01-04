44213bf4a42e04bb2eeeddc176451930c359a168
Sharon Zheng <zsharon012@gmail.com>
2025-01-03 18:34:52 -0600

    add key to .map and fix interface error

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,18 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-/* interface User {
-  id: number;
-  firstName: string;
-  lastName: string;
-  email: string;
-  bio: string;
-  major: string;
-  graduationYear: string;
-  profilePicture: string;
-  created_at: string;
-} */
-
 function NavBar(props) {
   return (
     <div class='nav'>
@@ -59,21 +47,6 @@ function Post(props) {
 }
 
 export default function Main() {
-
-/*   const [users, setUsers] = useState(null);
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchUsers = async () => {
-      const response = await fetch("https://disc-assignment-5-users-api.onrender.com/api/users")
-        .catch((err) => console.log("fetching user data failed"));
-      const data = await response.json();
-      setLoading(false);
-      setUsers(data);
-    };
-
-    fetchUsers();
-  }, []); */
 
   return (
     <div class="main">
--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,18 +1,6 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 
-interface User {
-    id: number;
-    firstName: string;
-    lastName: string;
-    email: string;
-    bio: string;
-    major: string;
-    graduationYear: string;
-    profilePicture: string;
-    created_at: string;
-}
-
 export default function FetchData() {
     const [users, setUsers] = useState(null);
     const [loading, setLoading] = useState(true);
@@ -35,7 +23,7 @@ export default function FetchData() {
         <div>
             <ul>
                 {users.map((list) => (
-                    <li>
+                    <li key={list.id}>
                         {list.id}{list.firstName}{list.lastName}{list.email}{list.bio}{list.major}{list.graduationYear}{list.profilePicture}{list.created_at}
                     </li>
                 ))}
