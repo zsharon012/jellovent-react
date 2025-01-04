8c1691b32e6aaab4f3715e99f902250f0f15914c
Sharon Zheng <zsharon012@gmail.com>
2025-01-03 19:32:43 -0600

    fix readability and styling

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,78 +1,21 @@
 import React from 'react';
-import { useState, useEffect } from 'react';
-
-function NavBar(props) {
-  return (
-    <div class='nav'>
-      <div class="icons"></div>
-      <h1>{props.title}</h1>
-      <div class="body"></div>
-      <div class="icons"></div>
-      <div class="icons"></div>
-      <div class="icons"></div>
-    </div>
-  )
-}
-
-function Post(props) {
-  const [mode, setLike] = useState('unliked');
-
-  const switchLike = () => {
-    if (mode == 'unliked') {
-      setLike('liked');
-    } else if (mode == 'liked') {
-      setLike('unliked');
-    }
-  }
-
-  useEffect(() => {
-    console.log(mode);
-  }, [mode]);
-
-  return (
-    <div class="profile_box">
-      <div class="name_space">
-        <div class="icons"></div>
-        <div>{props.name}</div>
-      </div>
-      <img src={props.image} class="image" alt="event post image" />
-      <div class="icon_space">
-        <div style={{ backgroundColor: mode == 'unliked' ? 'black' : 'lightpink' }} class="icons" onClick={switchLike}></div>
-        <div class="icons"></div>
-        <div class="icons"></div>
-      </div>
-      <div class="captions">{props.caption}</div>
-    </div>
-  )
-}
+import NavBar from "../NavBar.jsx";
+import Post from "../Post.jsx";
+import FilterBox from "../FilterBox.jsx";
+import Footer from "../Footer.jsx";
 
 export default function Main() {
-
   return (
     <div class="main">
       <NavBar title='jellovent' />
       <div class="search">
         <div class="container">
-          <div class="box">
-            <h2>Event Type</h2>
-            <h2 class="category">
-              event type name <br />
-              event type name <br />
-              event type name <br />
-              event type name<br />
-              event type name<br />
-              event type name<br />
-            </h2>
-          </div>
+          <FilterBox />
           <Post image="https://clicklovegrow.com/wp-content/uploads/2019/08/1-2.jpg" name="Sample Name 1" caption="Sample Caption 1" />
           <Post image="https://assets.mixbook.com/images/optimized/jpg/inside/1073/2560/3fa25_christian_bowen_w_Oe_DB_Kn5sdo_unsplash_4c13550ced.webp" name="Sample Name 2" caption="Sample Caption 2" />
         </div>
       </div>
-      <footer class="footer">
-        <div>
-          <h2 align="center">footer</h2>
-        </div>
-      </footer>
+      <Footer />
     </div>
   )
 }
---
+++ src/components/Post.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { useState, useEffect } from 'react';
+
+export default function Post(props) {
+    const [mode, setLike] = useState('unliked');
+  
+    const switchLike = () => {
+      if (mode == 'unliked') {
+        setLike('liked');
+      } else if (mode == 'liked') {
+        setLike('unliked');
+      }
+    }
+  
+    useEffect(() => {
+      console.log(mode);
+    }, [mode]);
+  
+    return (
+      <div class="profile_box">
+        <div class="name_space">
+          <div class="icons"></div>
+          <div>{props.name}</div>
+        </div>
+        <img src={props.image} class="image" alt="event post image" />
+        <div class="icon_space">
+          <div style={{ backgroundColor: mode == 'unliked' ? 'black' : 'lightpink' }} class="icons" onClick={switchLike}></div>
+          <div class="icons"></div>
+          <div class="icons"></div>
+        </div>
+        <div class="captions">{props.caption}</div>
+      </div>
+    )
+  }
